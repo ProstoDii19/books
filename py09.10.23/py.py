2a1601c43ba6c94fff0cabedb99c511fcbc1fbb2
Дианочка <118354497+ProstoDii19@users.noreply.github.com>
2023-10-09 10:23:10 +0300

    Add files via upload

--- py09.10.23/py.py
+++ py09.10.23/py.py
@@ -1,19 +1,23 @@
-# открываю файл с названиями книг
-with open('books.txt', 'r') as file:
-    # считываю все строки из файла
-    lines = file.readlines()
+def sort_books():
+    # открываю файл с названиями книг
+    with open('books.txt', 'r') as file:
+        # считываю все строки из файла
+        lines = file.readlines()
 
-# создаю список книг из считанных строк
-books = [line.strip() for line in lines]
+    # создаю список книг из считанных строк
+    books = [line.strip() for line in lines]
 
-# сортирую список книг по алфавиту
-sorted_books = sorted(books)
+    # сортирую список книг по алфавиту
+    sorted_books = sorted(books)
 
-# открываю файл для записи отсортированного списка
-with open('sorted_books.txt', 'w') as file:
-    # записываю каждую книгу из отсортированного списка на отдельной строке
-    for book in sorted_books:
-        file.write(book + '\n')
+    # открываю файл для записи отсортированного списка
+    with open('sorted_books.txt', 'w') as file:
+        # записываю каждую книгу из отсортированного списка на отдельной строке
+        for book in sorted_books:
+            file.write(book + '\n')
+
+# вызываю функцию для выполнения задачи
+sort_books()
 
 # Задание 1.
 """В текстовом файле 'books.txt' с новой строки хранятся названия книг.
